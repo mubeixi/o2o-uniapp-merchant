1a4123aa6537955c829ac9fb8946d783e5e7d411
mbx <mbx0829@163.com>
2020-01-14 08:37:39 +0800

    新增会员和商家入驻

---
+++ pages/person/vipCenter.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<div class="vip-center-top">
+			<image src="/static/person/vipTopBg.png" class="imgFull"></image>
+
+			<swiper class="center" :indicator-dots="false" :autoplay="false"  :duration="1000" :current="inds" @change="change">
+				<swiper-item class="vipFir"  >
+					<!-- v-for="(item,index) of pro.levels" :key="index"  :style="pro.levels.length==1?'margin-left:43rpx;':''"-->
+
+				</swiper-item>
+			</swiper>
+		</div>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.vip-center-top{
+	width: 750rpx;
+	height: 232px;
+	position: relative;
+}
+
+
+</style>
