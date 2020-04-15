151699c01f1366f5eb8c31364f052bffdeebd0f0
junxiangwang <271938312@qq.com>
2020-04-15 13:45:02 +0800

    refactor(uniapp): 利用uniapp开发
    1.使用uniapp开发 2.引用uni-ui组件库

---
+++ App.vue
@@ -0,0 +1,16 @@
+<style>
+	/*每个页面公共css */
+</style>
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
