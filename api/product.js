4c0218c41bbadcfdc796b4fe6ac91a5fcedcf495
mbx0829@163.com <mubeixi0829>
2020-04-18 14:49:02 +0800

    fix(商品详情): 修复商品详情门店页面和接口
    修复商品详情门店页面和接口

--- api/product.js
+++ api/product.js
@@ -14,5 +14,10 @@ export const getProductDetail = (param, options) => fetch({ act: 'prod_detail', 
 
 
 //获取活动详情
-// 获取产品详情
 export const getActiveInfo = (param, options) => fetch({ act: 'getActiveInfo', param, options })
+
+//获取商家详情和入驻信息
+export const getBizInfo = (param, options) => fetch({ act: 'getBizInfo', param, options })
+
+//获取商家列表
+export const getStoreList = (param, options) => fetch({ act: 'getStoreList', param, options })
