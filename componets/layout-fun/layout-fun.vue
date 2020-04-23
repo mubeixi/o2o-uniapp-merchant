0a6df5d20bb38ae23e23bda74b42b51ac540caa7
jiahaihong <527538629@qq.com>
2020-04-23 16:04:09 +0800

    feat(增加了个人中心页面，增加两个组件): 增加了个人中心页，增加了一个产品组件，一个展示功能图标的组件

---
+++ componets/layout-fun/layout-fun.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="item">
+		<LayoutIcon :rotate="rotate" :type="type" :weight="weight" :display="display" :color="color" :size="size"></LayoutIcon>
+		<div class="icon-name">{{name}}</div>
+	</div>
+</template>
+<script>
+import LayoutIcon from '@/componets/layout-icon/layout-icon'
+export default {
+  components: {
+    LayoutIcon
+  },
+	props: {
+    rotate: {
+      type: String,
+      default: '0deg'
+    },
+    type: {
+      require: true,
+      type: String,
+      default: ''
+    },
+    weight: {
+      type: String,
+      default: 'normal'
+    },
+    display: {
+      type: String,
+      default: 'block'
+    },
+    color: {
+      type: String,
+      default: '#666'
+    },
+    size: {
+      type: [Number, String],
+      default: 28
+    },
+		name: {
+      type: String,
+			default: ''
+		}
+	}
+}
+
+</script>
+<style lang="scss" scoped>
+	.item {
+		width: 25%;
+		text-align: center;
+		font-size: 24rpx;
+		color: #333;
+		margin-bottom: 67rpx;
+	}
+	.icon-name {
+		text-align: center;
+	}
+</style>
