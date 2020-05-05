805f67a458335300cd44506914d0ba0ff974e4f2
mbx0829@163.com <mubeixi0829>
2020-05-05 16:24:40 +0800

    feat(商品评论): 1.新增商品评论页面和接口2.修复订单列表的问题
    1.新增商品评论页面和接口2.修复订单列表的问题

--- pages/order/OrderList.vue
+++ pages/order/OrderList.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="page-wrap">
+    <div style="height: 100rpx;width: 750rpx"></div>
     <div class="navs">
       <div class="nav-item" :class="index==0?'active':''" @click="changIndex(0)">全部</div>
       <div class="nav-item" :class="index==1?'active':''" @click="changIndex(1)">
@@ -71,8 +72,8 @@
         </div>
       </div>
     </block>
-    
-    
+
+
     <div class="defaults" v-else>
       <image :src="'/static/client/empty.png'|domain"></image>
     </div>
@@ -191,7 +192,7 @@ export default {
       orderLsit.data.map(item => {
         item.Order_Shipping = JSON.parse(item.Order_Shipping)
       })
-      
+
       this.totalCount = orderLsit.totalCount
       if (item === 'init') {
         this.orderList = orderLsit.data
@@ -232,7 +233,7 @@ export default {
     margin-right: 10px;
     /*position:relative;*/
     z-index: 3;
-    
+
     .tooltip {
       position: absolute;
       background: #fff;
@@ -247,9 +248,9 @@ export default {
       border-radius: 5px;
       color: #444;
       font-size: 15px;
-      
+
       &::after {
-        
+
         content: " ";
         position: absolute;
         bottom: 100%; /* 提示工具头部 */
@@ -262,13 +263,13 @@ export default {
       }
     }
   }
-  
+
   .page-wrap {
     background-color: #ffffff !important;
     min-height: 100vh;
     padding-bottom: 50px;
   }
-  
+
   .titless {
     position: fixed;
     top: 0rpx;
@@ -276,7 +277,7 @@ export default {
     width: 100%;
     z-index: 999;
   }
-  
+
   .navs {
     z-index: 999;
     position: fixed;
@@ -294,13 +295,13 @@ export default {
     background: #fff;
     font-size: 28rpx;
     padding: 0 10px;
-    
+
     .nav-item {
       flex: 1;
       box-sizing: border-box;
       text-align: center;
       position: relative;
-      
+
       .jiaobiao {
         position: absolute;
         top: 24rpx;
@@ -317,64 +318,64 @@ export default {
         color: #F43131;
       }
     }
-    
+
     .nav-item.active {
       color: red;
       border-bottom: 2px solid red;
     }
   }
-  
+
   .order {
     padding: 0rpx 20rpx;
     background: #fff;
     position: relative;
-    
+
     .bizinfo {
       margin-top: 20rpx;
       display: flex;
       align-items: center;
       justify-content: space-between;
       width: 100%;
-      
+
       .img {
         width: 70rpx;
         height: 70rpx;
         border-radius: 50%;
         margin-right: 21rpx;
       }
-      
+
       .bizname {
         flex: 1;
         font-size: 28rpx;
       }
-      
+
       .status {
         color: red;
         font-size: 26rpx;
       }
     }
-    
+
     .pro {
       display: flex;
       margin-bottom: 50rpx;
       margin-top: 30rpx;
     }
-    
+
     .pro-msg {
       margin-left: 27rpx;
       width: 476rpx;
     }
-    
+
     .pro-div {
       width: 200rpx;
       height: 200rpx;
     }
-    
+
     .pro-img {
       width: 100%;
       height: 100%;
     }
-    
+
     .pro-name {
       font-size: 26rpx;
       margin-bottom: 20rpx;
@@ -385,7 +386,7 @@ export default {
       -webkit-line-clamp: 2;
       -webkit-box-orient: vertical;
     }
-    
+
     .attr {
       display: inline-block;
       height: 50rpx;
@@ -396,7 +397,7 @@ export default {
       padding: 0 20rpx;
       margin-bottom: 20rpx;
     }
-    
+
     .attrs {
       display: inline-block;
       height: 50rpx;
@@ -406,41 +407,41 @@ export default {
       padding: 0 20rpx;
       margin-bottom: 20rpx;
     }
-    
+
     .pro-price {
       color: #F43131;
       font-size: 36rpx;
     }
-    
-    
+
+
     .amount {
       font-size: 30rpx;
       float: right;
       color: #333;
     }
-    
+
     .total {
       font-size: 24rpx;
       padding: 40rpx 0rpx;
       margin-right: 15rpx;
-      
+
       .price {
         color: red;
         font-size: 30rpx;
-        
+
         span {
           font-size: 24rpx;
         }
       }
     }
-    
+
     .btn-group {
       display: flex;
       /*text-align: right;*/
       justify-content: flex-end;
       align-items: center;
       margin-bottom: 30rpx;
-      
+
       span {
         display: inline-block;
         //width: 150rpx;
@@ -452,11 +453,11 @@ export default {
         border-radius: 10rpx;
         color: #999;
         font-size: 26rpx;
-        
+
         &:last-child {
           margin-left: 14rpx;
         }
-        
+
         &.active {
           color: #fff;
           background: #F43131;
@@ -465,24 +466,24 @@ export default {
       }
     }
   }
-  
+
   .text-right {
     text-align: right;
   }
-  
+
   .defaults {
     margin: 0 auto;
     width: 640rpx;
     height: 480rpx;
     margin-top: 100rpx;
   }
-  
+
   .refuseApplyDialog {
     width: 560rpx;
     box-sizing: border-box;
     padding: 15px;
     font-size: 14px;
-    
+
     .reason {
       font-size: 14px;
       min-height: 200px;
@@ -492,7 +493,7 @@ export default {
       width: auto;
       padding: 10px;
     }
-    
+
     .inputs {
       font-size: 14px;
       border: 1px solid #E3E3E3;
@@ -502,19 +503,19 @@ export default {
       width: auto;
       margin-bottom: 10px;
     }
-    
+
     .reasons {
       min-height: 20px;
     }
-    
+
   }
-  
+
   .control {
     width: 100%;
     margin-top: 15px;
     display: flex;
     border-top: 1px solid #e4e4e4;
-    
+
     .action-btn {
       flex: 1;
       font-size: 16px;
@@ -523,9 +524,9 @@ export default {
       line-height: 40px;
       text-align: center;
     }
-    
+
   }
-  
+
   .my-huo {
     margin-top: 20px;
     margin-bottom: 10px;
@@ -535,14 +536,14 @@ export default {
     width: 100%;
     text-align: center;
   }
-  
+
   .my-content {
     font-size: 14px;
     width: 100%;
     text-align: center;
     color: #a1a1a1;
   }
-  
+
   .page-wrap /deep/ .popup-content {
     padding: 0px;
   }
