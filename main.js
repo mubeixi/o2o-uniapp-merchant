2e5d4f08600668e8eacc0774ef0474e6b2c06b65
junxiangwang <271938312@qq.com>
2020-05-20 10:51:23 +0800

    fix(精简文件): 优化moment.js的引入的方式
    1.精简大小 2.解决报未加载语言包的bug

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ import locales from './common/langue/locales'
 import './common'
 import store from '@/store'
 
-
-import moment from 'moment/min/moment-with-locales.min'
+import moment from './libs/moment.min'
 uni.$moment = moment
 
 Vue.config.productionTip = false
