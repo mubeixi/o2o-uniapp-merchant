12f5ed4d39355e536ccfc217d385383d8cc382b4
mbx0829@163.com <mubeixi0829>
2020-07-06 13:56:15 +0800

    增加env注释

--- common/env.js
+++ common/env.js
@@ -11,6 +11,12 @@ export const apiBaseUrl = 'https://newo2o.bafangka.com'
 export const staticUrl = 'https://newo2o.bafangka.com'
 export const users_id = 'wkbq6nc2kc'
 
+// O2O正式版IM
+//appid : dkujHdxulyJkmxh
+//appsecret : b1935ddee886db0a9877876051b42846
+// O2O测试版IM
+//appid : uLwqowvvkHAUmq5
+//appsecret : 4MJB9wQDO3EpxSdIPzLn6kejsqnv5d6e
 
 //测试appid   wxfe5d2621576cf895  域名 https://newo2o.bafangka.com
 //正式appid   wx28e6e80223bd5f0f  域名 https://newo2o.netcnnet.net
