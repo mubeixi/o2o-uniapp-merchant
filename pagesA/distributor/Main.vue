9a0080e960d751db2e2229e70c6bc48f08669e3e
junxiang <271938312@qq.com>
2020-07-24 14:48:10 +0800

    style 整理代码

--- pagesA/distributor/Main.vue
+++ pagesA/distributor/Main.vue
@@ -15,10 +15,10 @@
           {{data.disInfo.Level_Name||''}}
           <layout-icon class="rightMy" color="#000" display="inline" size="14"
                        type="iconicon-arrow-right" v-if="data.disInfo.Level_Name"></layout-icon>
-        
+
         </view>
       </div>
-      
+
       <view @click="goLogin" class="fz-14 loginBtn" plain size="mini" v-if="!userInfo.User_ID">登录/注册</view>
       <view @click="goDistributor" class="fz-14 loginBtn" plain size="mini"
             v-if="userInfo.User_ID && userInfo.Is_Distribute!==1">成为{{initData.commi_rename.commi}}
@@ -29,7 +29,7 @@
             累计业绩（元）
             <layout-icon class="rightMys" color="#000" display="inline" size="14"
                          type="iconicon-arrow-right"></layout-icon>
-          
+
           </view>
           <view class="salesSumPrice" v-if="userInfo.User_ID&&userInfo.Is_Distribute">
             {{data.total_sales}}
@@ -51,7 +51,7 @@
           </view>
         </view>
       </view>
-    
+
     </view>
     <view class="center" v-if="userInfo.User_ID&&userInfo.Is_Distribute">
       <view>可提现金额</view>
@@ -179,7 +179,7 @@ export default {
     goOther (item) {
       if (!checkIsLogin(1, 1)) return
       if (!checkIsDistribute(1, 1)) return
-      
+
       uni.navigateTo({
         url: routerList[item.url]
       })
@@ -213,17 +213,17 @@ export default {
   .all {
     background-color: #f8f8f8;
   }
-  
+
   .top {
     width: 750rpx;
     height: 400rpx;
     position: relative;
-    
+
     .image {
       width: 100%;
       height: 100%;
     }
-    
+
     .title {
       width: 138rpx;
       font-size: 34rpx;
@@ -233,7 +233,7 @@ export default {
       top: 27rpx;
       left: 306rpx;
     }
-    
+
     .msg {
       width: 45rpx;
       height: 45rpx;
@@ -241,7 +241,7 @@ export default {
       top: 25rpx;
       right: 21rpx;
     }
-    
+
     .person {
       width: 92rpx;
       height: 92rpx;
@@ -249,13 +249,13 @@ export default {
       top: 109rpx;
       left: 329rpx;
       border-radius: 50%;
-      
+
       .image {
         width: 100%;
         height: 100%;
       }
     }
-    
+
     .nickName {
       font-size: 28rpx;
       height: 27rpx;
@@ -268,7 +268,7 @@ export default {
       left: 175rpx;
       text-align: center;
     }
-    
+
     .loginBtn {
       padding: 4px 10px;
       color: white;
@@ -279,7 +279,7 @@ export default {
       top: 50%;
       transform: translate(-50%, -50%);
     }
-    
+
     .sales {
       width: 690rpx;
       height: 160rpx;
@@ -290,18 +290,18 @@ export default {
       box-shadow: 0px 0px 27rpx 0px rgba(244, 49, 49, 0.46);
       border-radius: 10rpx;
       display: flex;
-      
+
       .left, view.right {
         width: 50%;
         margin-top: 42rpx;
         margin-bottom: 41rpx;
         text-align: center;
       }
-      
+
       .left {
         border-right: 1px solid #E7E7E7;
       }
-      
+
       .salesSum {
         height: 25rpx;
         font-size: 26rpx;
@@ -309,7 +309,7 @@ export default {
         font-weight: 500;
         color: rgba(51, 51, 51, 1);
       }
-      
+
       .salesSumPrice {
         height: 29rpx;
         font-size: 38rpx;
@@ -320,7 +320,7 @@ export default {
       }
     }
   }
-  
+
   .center {
     width: 690rpx;
     height: 90rpx;
@@ -332,7 +332,7 @@ export default {
     display: flex;
     align-items: center;
     position: relative;
-    
+
     & view:first-child {
       margin-left: 48rpx;
       color: #333333;
@@ -340,13 +340,13 @@ export default {
       font-size: 26rpx;
       margin-right: 16rpx;
     }
-    
+
     & view:nth-child(2) {
       font-size: 34rpx;
       font-weight: bold;
       color: #F43131;
     }
-    
+
     & view:nth-child(3) {
       font-size: 26rpx;
       font-weight: 500;
@@ -362,19 +362,19 @@ export default {
       top: 23rpx;
     }
   }
-  
+
   .last {
     width: 691rpx;
     height: 668rpx;
     margin: 0 auto;
     position: relative;
     margin-top: 30rpx;
-    
+
     image.back {
       width: 100%;
       height: 100%;
     }
-    
+
     .zhezhao {
       position: absolute;
       top: 0;
@@ -388,7 +388,7 @@ export default {
       box-sizing: border-box;
       display: flex;
       flex-wrap: wrap;
-      
+
       .td {
         width: 209rpx;
         height: 222rpx;
@@ -396,13 +396,13 @@ export default {
         border-bottom: 1px dotted #D3D3D3;
         text-align: center;
         box-sizing: border-box;
-        
+
         .imgs {
           width: 95rpx;
           height: 95rpx;
           margin-top: 44rpx;
         }
-        
+
         .views {
           height: 25rpx;
           line-height: 25rpx;
@@ -414,7 +414,7 @@ export default {
       }
     }
   }
-  
+
   .putong {
     height: 50rpx;
     line-height: 50rpx;
@@ -429,7 +429,7 @@ export default {
     display: flex;
     align-items: center;
     padding-left: 10px;
-    
+
   }
 
 </style>
